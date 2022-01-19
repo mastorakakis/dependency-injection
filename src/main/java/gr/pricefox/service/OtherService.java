fb5698455f901353cf68fab8ed97439e6ed02ccf
mastorakakis <panagiotismast@gmail.com>
2022-01-19 05:53:44 +0200

    add more repositories

--- src/main/java/gr/pricefox/service/OtherService.java
+++ src/main/java/gr/pricefox/service/OtherService.java
@@ -1,4 +1,15 @@
 package gr.pricefox.service;
 
+import gr.pricefox.repository.OtherRepository;
+
 public class OtherService {
+
+    private OtherRepository otherRepository;
+
+    public OtherService() {
+    }
+
+    public OtherService(OtherRepository otherRepository) {
+        this.otherRepository = otherRepository;
+    }
 }
