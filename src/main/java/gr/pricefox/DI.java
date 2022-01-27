e60d64513b0706273d433cc06dc10c6bcaa23853
Panagiotis Mastorakakis <panagiotismast@gmail.com>
2022-01-27 02:30:46 +0200

    delete classListOf

--- src/main/java/gr/pricefox/DI.java
+++ src/main/java/gr/pricefox/DI.java
@@ -66,47 +66,6 @@ public class DI {
                 .map(theInterface::cast)
                 .collect(Collectors.toList());
     }
-
-    public <T> List<Class<T>> classListOf(Class<T> theInterface) {
-        List<Class<T>> subClassesOfInterface = new ArrayList<>();
-
-        for (String packageName : getAllPackageNames()) {
-            Set<Class<?>> classes = findAllClassesUsingClassLoader(packageName);
-            for (Class<?> classElement: classes) {
-                if (Arrays.asList(classElement.getInterfaces()).contains(theInterface)) {
-                    subClassesOfInterface.add((Class<T>) classElement);
-                }
-            }
-        }
-        return subClassesOfInterface;
-    }
-
-    public List<String> getAllPackageNames() {
-        return Arrays.stream(Package.getPackages())
-                .map(Package::getName)
-                .filter(s -> s.startsWith(this.getClass().getPackageName()))
-                .collect(Collectors.toList());
-    }
-
-    public Set<Class<?>> findAllClassesUsingClassLoader(String packageName) {
-        InputStream stream = ClassLoader.getSystemClassLoader()
-                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        return reader.lines()
-                .filter(line -> line.endsWith(".class"))
-                .map(line -> getClass(line, packageName))
-                .collect(Collectors.toSet());
-    }
-
-    private Class<?> getClass(String className, String packageName) {
-        try {
-            return Class.forName(packageName + "."
-                    + className.substring(0, className.lastIndexOf('.')));
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 }
 
 
